acd768c6e678062d17578952c5d7f7f6113e86c6
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-05-08 02:54:12 -0700

    fixed errors in push.c

--- push.c
+++ push.c
@@ -8,36 +8,47 @@
 void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new_node;
+	char *token;
 	int num;
-
+	
 	if (!stack)
 	{
 		fprintf(stderr, "L%d: Stack not found\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	if (!isdigit(stack_token))
+	token = strtok(NULL, " \n");
+	if (!token)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	num = atoi(stack_token);
+	for (int i = 0; token[i]; i++)
+	{
+		if (i == 0 && token[i] == '-')
+		{
+			continue;
+		}
+		if (!isdigit(token[i]))
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+	}
+	num = atoi(token);
 	new_node = malloc(sizeof(stack_t));
 	if (!new_node)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-
 	new_node->n = num;
 	new_node->prev = NULL;
-
 	if (*stack)
 	{
 		new_node->next = *stack;
 		(*stack)->prev = new_node;
 	}
-
 	*stack = new_node;
 }
